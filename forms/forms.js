90b71dfbc22cfbc3565e9b5b236619b49c3e20e1
Will Danner IV <willdanneriv@gmail.com>
2026-04-16 19:17:50 -0400

    feat: Combined forms to be in one forms.js file. Removed individual folders and files.

---
+++ forms/forms.js
@@ -0,0 +1,176 @@
+window.ffFormData = {
+    COUNTRIES: {
+        AUS: {
+            VEOC_Closures: {
+                "name": "AUS VEOC closures",
+                "url": "https://docs.google.com/forms/d/e/1FAIpQLScxs_FMsjcEeTD0gjWUahb3Qb0XjeB2K32n_vWLm_HV2fUweQ/viewform",
+                "url_with_keys": "https://docs.google.com/forms/d/e/1FAIpQLScxs_FMsjcEeTD0gjWUahb3Qb0XjeB2K32n_vWLm_HV2fUweQ/viewform?entry.1553765347=username&entry.1264424583=CLOSED&entry.1811077109=permalink&entry.792657790=Two-Way&entry.345142186=reason&entry.1102521735=2016-09-20+03:00&entry.2015424420=street+name&entry.1547375393=from+street&entry.1335391716=to+street&entry.1867193205=NSW&entry.1714138473=county&entry.1803937317=source&entry.1648634142=notes",
+                "fields": {
+                    "username": "1553765347",
+                    "closureStatus": "1264424583",
+                    "permalink": "1811077109",
+                    "closedDir": "792657790",
+                    "closedReason": "345142186",
+                    "endDate": "1102521735",
+                    "streetname": "2015424420",
+                    "fromStreet": "1547375393",
+                    "toStreet": "1335391716",
+                    "stateabbr": "1867193205",
+                    "county": "1714138473",
+                    "source": "1803937317",
+                    "notes": "1648634142"
+                }
+            }
+        },
+        USA: {
+            JANE_TTS: {
+                "name": "US Jane TTS Pronunciation",
+                "url": "https://docs.google.com/forms/d/e/1FAIpQLSeuCmC0zy7GEQDJQP5R8dndxYhXCkqzadrPgP89BvatVl1bdg/viewform",
+                "url_with_keys": "",
+                "fields": {
+                    "username": "324217272",
+                    "state": "1065619417",
+                    "issue": "1086951221",
+                    "streetname": "1163516948",
+                    "incorrectp": "1191620241",
+                    "correctp": "1649051316",
+                    "permalink": "2028167849",
+                    "instructions": "2120232339",
+                    "comments": "1917392591"
+                }
+            },
+            VEOC_Closures: {
+                "name": "USA VEOC closures",
+                "url": "https://docs.google.com/forms/d/e/1FAIpQLScX8hlXopzXTLEPUpXwdXBWF6qFmz1asKtX5IOy9gbdsfMmTA/viewform",
+                "url_with_keys": "https://docs.google.com/forms/d/e/1FAIpQLScX8hlXopzXTLEPUpXwdXBWF6qFmz1asKtX5IOy9gbdsfMmTA/viewform?entry.1553765347=username&entry.1264424583=CLOSED&entry.1811077109=permalink&entry.792657790=Two-Way&entry.345142186=reason&entry.1102521735=2016-09-20+03:00&entry.2015424420=street+name&entry.1547375393=from+street&entry.1335391716=to+street&entry.1867193205=SC&entry.1714138473=county&entry.1803937317=source&entry.1648634142=notes",
+                "fields": {
+                    "username": "1553765347",
+                    "closureStatus": "1264424583",
+                    "permalink": "1811077109",
+                    "closedDir": "792657790",
+                    "closedReason": "345142186",
+                    "endDate": "1102521735",
+                    "streetname": "2015424420",
+                    "fromStreet": "1547375393",
+                    "toStreet": "1335391716",
+                    "stateabbr": "1867193205",
+                    "county": "1714138473",
+                    "source": "1803937317",
+                    "notes": "1648634142"
+                }
+            },
+            STATES: {
+                IL: {
+                    IL_Closures: {
+                        "name": "IL closures",
+                        "url": "https://docs.google.com/forms/d/e/1FAIpQLSff7nsBw8qxCojBdxrjTPl6tercqyyzGy92Vif_SBdHkYDchw/viewform",
+                        "url_with_keys": "https://docs.google.com/forms/d/e/1FAIpQLSff7nsBw8qxCojBdxrjTPl6tercqyyzGy92Vif_SBdHkYDchw/viewform?entry.1204781462=Reporter&entry.828228572=Reported&entry.1647952662=Street+name+&entry.1501712688=From+street+&entry.2094306654=To+street+&entry.1414240321=Two-Way&entry.900957975=10/27/2016+00:00&entry.1051351191=Adams&entry.1093044522=City+&entry.1540676081=IDOT&entry.430378754=Reason+&entry.1754051160=Permalink+&entry.172235277=Source+&entry.1722909714=Notes",
+                        "fields": {
+                            "username": "1204781462",
+                            "closureStatus": "828228572",
+                            "permalink": "1754051160",
+                            "closedDir": "1414240321",
+                            "closedReason": "430378754",
+                            "endDate": "900957975",
+                            "streetname": "1647952662",
+                            "fromStreet": "1501712688",
+                            "toStreet": "2094306654",
+                            "county": "1051351191",
+                            "city": "1093044522",
+                            "source": "172235277",
+                            "notes": "1722909714"
+                        }
+                    },
+                },
+                OK: {
+                    OK_Closures: {
+                        "name": "OK closures",
+                        "url": "https://docs.google.com/forms/d/e/1FAIpQLSd8wouEY6DhA4ifMb_Ub0rEE7X4TCM7WIEqFiT4Lq1hySxEWw/viewform",
+                        "url_with_keys": "https://docs.google.com/forms/d/e/1FAIpQLSd8wouEY6DhA4ifMb_Ub0rEE7X4TCM7WIEqFiT4Lq1hySxEWw/viewform?entry.1553765347=username&entry.1264424583=CLOSED&entry.1811077109=permalink&entry.792657790=Two-Way&entry.345142186=reason&entry.1102521735=2016-09-20+03:00&entry.2015424420=street+name&entry.1547375393=from+street&entry.1335391716=to+street&entry.1867193205=OK&entry.1714138473=county&entry.1803937317=source&entry.1648634142=notes",
+                        "fields": {
+                            "username": "1553765347",
+                            "closureStatus": "1264424583",
+                            "permalink": "1811077109",
+                            "closedDir": "792657790",
+                            "closedReason": "345142186",
+                            "endDate": "1102521735",
+                            "streetname": "2015424420",
+                            "fromStreet": "1547375393",
+                            "toStreet": "1335391716",
+                            "stateabbr": "1867193205",
+                            "county": "1714138473",
+                            "source": "1803937317",
+                            "notes": "1648634142"
+                        }
+                    }
+                },
+                VA: {
+                    VA_Closures: {
+                        "name": "VA Closures",
+                        "url": "https://docs.google.com/forms/d/e/1FAIpQLScwEyNVqiHHdFjc4hr82zlFXW2bAsff9pqIzFUqT8Evh6YROg/viewform",
+                        "url_with_key": "https://docs.google.com/forms/d/e/1FAIpQLScwEyNVqiHHdFjc4hr82zlFXW2bAsff9pqIzFUqT8Evh6YROg/viewform?usp=pp_url&entry.1553765347=kwrigh01&entry.1264424583=CLOSED&entry.1811077109=https://www.waze.com/editor/?env%3Dusa%26lon%3D-79.99979%26lat%3D37.89567%26zoom%3D5%26segments%3D82457306,82457308,82457338&entry.792657790=Two-Way&entry.345142186=Bridge+Work&entry.1102521735=2018-07-01+23:59&entry.2015424420=Morris+Hollow+Rd&entry.1547375393=1st&entry.1335391716=2nd&entry.1867193205=VA&entry.1714138473=Alleghany&entry.1803937317=VDOT&entry.1648634142=Test+Closure",
+                        "fields": {
+                            "username": "1553765347",
+                            "closureStatus": "1264424583",
+                            "permalink": "1811077109",
+                            "closedDir": "792657790",
+                            "closedReason": "345142186",
+                            "endDate": "1102521735",
+                            "streetname": "2015424420",
+                            "fromStreet": "1547375393",
+                            "toStreet": "1335391716",
+                            "stateabbr": "1867193205",
+                            "county": "1714138473",
+                            "source": "1803937317",
+                            "notes": "1648634142"
+                        }
+                    }
+                },
+                WI: {
+                    WI_Closures: {
+                        "name": "WI closures",
+                        "url": "https://docs.google.com/forms/d/e/1FAIpQLSeiKY0KsO0xN69Asw77MARQFmxOy6zQXF-k2OQdWOfwtiCp7Q/viewform",
+                        "url_with_keys": "https://docs.google.com/forms/d/e/1FAIpQLSeiKY0KsO0xN69Asw77MARQFmxOy6zQXF-k2OQdWOfwtiCp7Q/viewform?entry.1204781462=ojlaw&entry.828228572=CLOSED&entry.1647952662=Test1&entry.1501712688=Test2&entry.2094306654=Test3&entry.1414240321=One-Way&entry.900957975=00/00/0000+00:00&entry.1051351191=Adams&entry.1093044522=Test4&entry.1540676081=City&entry.430378754=Test5&entry.1754051160=Test6&entry.172235277=Test7&entry.1722909714=Test8",
+                        "fields": {
+                            "username": "1204781462",
+                            "closureStatus": "828228572",
+                            "permalink": "1754051160",
+                            "closedDir": "1414240321",
+                            "closedReason": "430378754",
+                            "endDate": "900957975",
+                            "streetname": "1647952662",
+                            "fromStreet": "1501712688",
+                            "toStreet": "2094306654",
+                            "county": "1051351191",
+                            "city": "1093044522",
+                            "source": "172235277",
+                            "notes": "1722909714"
+                        }
+                    }
+                },
+                WV: {
+                    WV_Closures: {
+                        "name": "WV Closures",
+                        "url": "https://docs.google.com/forms/d/e/1FAIpQLSd2Stl1DabZRVZ315HT6kdF78e9ysqjItR09pYtE7WiKmPotg/viewform",
+                        "url_with_keys": "https://docs.google.com/forms/d/e/1FAIpQLSd2Stl1DabZRVZ315HT6kdF78e9ysqjItR09pYtE7WiKmPotg/viewform?usp=pp_url&entry.1677745178=user&entry.1292331655=CLOSED&entry.408708691=http://waze.com&entry.52585226=Two-Way&entry.1079033335=reason&entry.1176076628=2020-05-05+05:00&entry.223275032=street&entry.1159260930=from&entry.1582819664=to&entry.2135402288=Monongalia&entry.858860694=source&entry.1440969572=notes",
+                        "fields": {
+                            "username": "1677745178",
+                            "closureStatus": "1292331655",
+                            "permalink": "408708691",
+                            "closedDir": "52585226",
+                            "closedReason": "1079033335",
+                            "endDate": "1176076628",
+                            "streetname": "223275032",
+                            "fromStreet": "1159260930",
+                            "toStreet": "1582819664",
+                            "county": "2135402288",
+                            "source": "858860694",
+                            "notes": "1440969572"
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
